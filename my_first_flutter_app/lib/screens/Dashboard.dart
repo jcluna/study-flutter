bf34c5dd6c7b61ded89d4993e8776e15189ab4ce
Juan C. Luna H. <jc.luna@live.com.mx>
2023-09-30 21:28:26 -0600

    Image in Flutter (AssetImage, NetworkImage, Image.asset, Image.network)

--- my_first_flutter_app/lib/screens/Dashboard.dart
+++ my_first_flutter_app/lib/screens/Dashboard.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -12,11 +10,23 @@ class Dashboard extends StatelessWidget {
         title: Text('Dashboard'.toUpperCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
-        child: Text("Random Value is: ${getNumber()}"),
+      body: Image.asset(
+        "images/coding_3d.png",
+        height: 400.0,
+        fit: BoxFit.contain,
       ),
+      /*
+      body: const Image(
+        // image by asset
+        // image: AssetImage("images/coding_3d.png"),
+        // image by network
+        image: NetworkImage(
+            "https://i.pinimg.com/736x/43/5b/cc/435bcc37144b3a166b0c5c2ee8850058.jpg"),
+        width: 200.0,
+        height: 800.0,
+        fit: BoxFit.cover,
+      ),
+      */
     );
   }
 }
-
-int getNumber() => Random().nextInt(100);
